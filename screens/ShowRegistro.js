918d334f485e43227b0646bca0879cf14bede8a9
adrmadfer <adrmadfer@alum.us.es>
2022-06-06 19:10:19 +0200

    select date

--- screens/ShowRegistro.js
+++ screens/ShowRegistro.js
@@ -6,7 +6,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import {FlatList, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import Layout from "./Layout";
-import jsPDF from "jspdf";
+import DatePicker from 'react-native-datepicker';
 
 
 const ShowRegistro = ({navigation, route}) => {
@@ -21,6 +21,24 @@ const ShowRegistro = ({navigation, route}) => {
 
     const fechaString = fechaSeleccionada.toLocaleDateString();
     const mes = fechaSeleccionada.getMonth()+1;
+
+    let [isVisible, setIsVisible] = useState(false)
+
+
+    const handlePicker = () => {
+        setIsVisible(false)
+    }
+
+    const showPicker = () => {
+        setIsVisible(true)
+    }
+
+
+    const hidePicker = () => {
+        setIsVisible(false)
+    }
+
+
 
     useEffect(async () => {
         const token = await AsyncStorage.getItem("accessToken")
@@ -69,74 +87,7 @@ const ShowRegistro = ({navigation, route}) => {
 
                    */
 
-    const generatePDF = () => {
 
-        console.log(registrosMes)
-        var doc = new jsPDF();
-
-        for(var i = 0; i < registrosMes.length; i++) {
-
-            const reg = registrosMes.at(i)
-
-            const desayuno = (reg.desayuno === null) ? "" : reg.desayuno;
-            const almuerzo = (reg.almuerzo === null) ? "" : reg.almuerzo;
-            const merienda = (reg.merienda === null) ? "" : reg.merienda;
-            const cena = (reg.cena === null) ? "" : reg.cena;
-            const pasosDiarios = (reg.pasosDiarios === null) ? "" : reg.pasosDiarios;
-            const actividadFisica = (reg.actividadFisica === null) ? "" : reg.actividadFisica;
-            const tiempoAireLibre = (reg.tiempoAireLibre === null) ? "" : reg.tiempoAireLibre;
-            const horasSueno = (reg.horasSueno === null) ? "" : reg.horasSueno;
-            const relacionSocial = (reg.relacionSocial === null) ? "" : reg.relacionSocial;
-            const medicacionManana = (reg.medicacionManana === false) ? "No" : "Si";
-            const medicacionTarde = (reg.medicacionTarde === false) ? "No" : "Si";
-            const medicacionNoche = (reg.medicacionNoche === false) ? "NO" : "Si";
-
-            var splitDesayuno = doc.splitTextToSize(desayuno, 180)
-            var splitMerienda = doc.splitTextToSize(merienda, 180)
-            var splitCena = doc.splitTextToSize(cena, 180)
-            var splitAlmuerzo = doc.splitTextToSize(almuerzo, 180)
-            var splitActividad = doc.splitTextToSize(actividadFisica, 180)
-            var splitRelacionSocial = doc.splitTextToSize(relacionSocial, 180)
-
-            doc.setFontSize(14)
-            doc.text(20, 25, 'Nombre: ' + personaDependiente.nombre +    '  Apellidos: ' + personaDependiente.apellidos)
-            doc.setFontSize(25)
-            doc.text(55, 35, 'Registro día: ' + reg.fecha)
-            doc.setFontSize(10)
-            doc.text(20, 45, 'ALIMENTACIÓN')
-            doc.line(20, 47, 180, 47)
-            doc.text(20, 52, 'Desayuno: ')
-            doc.text(20, 57,  splitDesayuno)
-            doc.text(20, 75, 'Almuerzo: ')
-            doc.text(20, 80,  splitAlmuerzo)
-            doc.text(20, 100, 'Merienda: ')
-            doc.text(20, 105,  splitMerienda)
-            doc.text(20, 120, 'Cena: ')
-            doc.text(20, 125,  splitCena)
-            doc.text(20, 150, 'MEDICACIÓN')
-            doc.line(20, 152, 180, 152)
-            doc.text(20, 157, '¿Ha tomado la medicación correspondiente a la mañana?:  ' + medicacionManana)
-            doc.text(20, 162, '¿Ha tomado la medicación correspondiente a la tarde?: ' + medicacionTarde)
-            doc.text(20, 167, '¿Ha tomado la medicación correspondiente a la noche?: ' + medicacionNoche)
-            doc.text(20, 178, 'ACTIVIDAD FÍSICA')
-            doc.line(20, 180, 180, 180)
-            doc.text(20, 185, 'Actividad física realizada:')
-            doc.text(20, 190, splitActividad)
-            doc.text(20, 205, 'Pasos diarios: ' + pasosDiarios)
-            doc.text(20, 213, 'OTROS')
-            doc.line(20, 215, 180, 215)
-            doc.text(20, 220, 'Horas de sueño: ' + horasSueno)
-            doc.text(20, 225, 'Tiempo al aire libre: ' + tiempoAireLibre)
-            doc.text(20, 240, 'Relaciones sociales: ')
-            doc.text(20, 245, splitRelacionSocial)
-
-
-            doc.addPage()
-
-        }
-
-        doc.save('Demo.pdf')
-    }
 
 
     return (
@@ -144,8 +95,19 @@ const ShowRegistro = ({navigation, route}) => {
             <>
                 <Layout>
 
-                    <View>
-                        <Text>{fechaString}</Text>
+                    <View style={styles.container}>
+                        <Text>Seleccionar fecha :</Text>
+                        <DatePicker
+                            date={fechaSeleccionada}
+                            mode="date"
+                            placeholder="select date"
+                            format="DD/MM/YYYY"
+                            confirmBtnText="Aceptar"
+                            cancelBtnText="Cancelar"
+                            onDateChange={(date) => {
+                                setFechaSeleccionada(date);
+                            }}
+                        />
                     </View>
 
 
@@ -169,15 +131,6 @@ const ShowRegistro = ({navigation, route}) => {
                     <div className="no-registro">
                         NO HAY NINGÚN REGISTRO
                     </div>
-
-                    }
-
-
-                    { ((authState.rol === "FAMILIAR") || (authState.rol === "COORDINADOR")) &&
-
-                    <TouchableOpacity style={styles.buscarButton} onPress={generatePDF}>
-                        <Text style={styles.itemTitle}>Descargar registros del mes</Text>
-                    </TouchableOpacity>
 
                     }
 
